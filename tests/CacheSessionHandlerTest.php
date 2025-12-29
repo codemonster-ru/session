5d9fca321f19ef2567098c652a707e2141477e9b
KolesnikovKirill <kolesnikov-kiril@mail.ru>
2025-12-29 23:11:44 +0700

    fix(tests): align cache stub signatures with PSR-16 to stabilize test runs

--- tests/CacheSessionHandlerTest.php
+++ tests/CacheSessionHandlerTest.php
@@ -29,17 +29,7 @@ class CacheSessionHandlerTest extends TestCase
 
     public function testRetryOnFailure(): void
     {
-        $cache = new class extends ArrayCache {
-            private int $tries = 0;
-            public function set($key, $value, $ttl = null)
-            {
-                $this->tries++;
-                if ($this->tries === 1) {
-                    throw new \RuntimeException('fail');
-                }
-                return parent::set($key, $value, $ttl);
-            }
-        };
+        $cache = new FlakyCache();
 
         $handler = new CacheSessionHandler($cache, 'sess_', 0, 1, 0);
         $this->assertTrue($handler->write('abc', 'value'));
@@ -49,38 +39,38 @@ class CacheSessionHandlerTest extends TestCase
 /**
  * @psalm-suppress MethodSignatureMismatch
  */
-class ArrayCache implements CacheInterface
+trait ArrayCacheStorage
 {
     /** @var array<string, mixed> */
     private array $data = [];
 
-    public function get($key, $default = null)
+    private function getValue(string $key, mixed $default = null): mixed
     {
         return $this->data[$key] ?? $default;
     }
 
-    public function set($key, $value, $ttl = null)
+    private function setValue(string $key, mixed $value): bool
     {
         $this->data[$key] = $value;
 
         return true;
     }
 
-    public function delete($key)
+    private function deleteValue(string $key): bool
     {
         unset($this->data[$key]);
 
         return true;
     }
 
-    public function clear()
+    private function clearValues(): bool
     {
         $this->data = [];
 
         return true;
     }
 
-    public function getMultiple($keys, $default = null)
+    private function getValues(iterable $keys, mixed $default = null): iterable
     {
         $result = [];
         foreach ($keys as $key) {
@@ -90,7 +80,7 @@ class ArrayCache implements CacheInterface
         return $result;
     }
 
-    public function setMultiple($values, $ttl = null)
+    private function setValues(iterable $values): bool
     {
         foreach ($values as $key => $value) {
             $this->data[$key] = $value;
@@ -99,7 +89,7 @@ class ArrayCache implements CacheInterface
         return true;
     }
 
-    public function deleteMultiple($keys)
+    private function deleteValues(iterable $keys): bool
     {
         foreach ($keys as $key) {
             unset($this->data[$key]);
@@ -108,8 +98,133 @@ class ArrayCache implements CacheInterface
         return true;
     }
 
-    public function has($key)
+    private function hasValue(string $key): bool
     {
         return array_key_exists($key, $this->data);
+    }
+}
+
+$method = new ReflectionMethod(CacheInterface::class, 'get');
+$typedSignatures = $method->hasReturnType() || $method->getParameters()[0]->hasType();
+
+if ($typedSignatures) {
+    class ArrayCache implements CacheInterface
+    {
+        use ArrayCacheStorage;
+
+        public function get(string $key, mixed $default = null): mixed
+        {
+            return $this->getValue($key, $default);
+        }
+
+        public function set(string $key, mixed $value, null|int|\DateInterval $ttl = null): bool
+        {
+            return $this->setValue($key, $value);
+        }
+
+        public function delete(string $key): bool
+        {
+            return $this->deleteValue($key);
+        }
+
+        public function clear(): bool
+        {
+            return $this->clearValues();
+        }
+
+        public function getMultiple(iterable $keys, mixed $default = null): iterable
+        {
+            return $this->getValues($keys, $default);
+        }
+
+        public function setMultiple(iterable $values, null|int|\DateInterval $ttl = null): bool
+        {
+            return $this->setValues($values);
+        }
+
+        public function deleteMultiple(iterable $keys): bool
+        {
+            return $this->deleteValues($keys);
+        }
+
+        public function has(string $key): bool
+        {
+            return $this->hasValue($key);
+        }
+    }
+
+    class FlakyCache extends ArrayCache
+    {
+        private int $tries = 0;
+
+        public function set(string $key, mixed $value, null|int|\DateInterval $ttl = null): bool
+        {
+            $this->tries++;
+            if ($this->tries === 1) {
+                throw new \RuntimeException('fail');
+            }
+
+            return parent::set($key, $value, $ttl);
+        }
+    }
+} else {
+    class ArrayCache implements CacheInterface
+    {
+        use ArrayCacheStorage;
+
+        public function get($key, $default = null)
+        {
+            return $this->getValue((string) $key, $default);
+        }
+
+        public function set($key, $value, $ttl = null)
+        {
+            return $this->setValue((string) $key, $value);
+        }
+
+        public function delete($key)
+        {
+            return $this->deleteValue((string) $key);
+        }
+
+        public function clear()
+        {
+            return $this->clearValues();
+        }
+
+        public function getMultiple($keys, $default = null)
+        {
+            return $this->getValues($keys, $default);
+        }
+
+        public function setMultiple($values, $ttl = null)
+        {
+            return $this->setValues($values);
+        }
+
+        public function deleteMultiple($keys)
+        {
+            return $this->deleteValues($keys);
+        }
+
+        public function has($key)
+        {
+            return $this->hasValue((string) $key);
+        }
+    }
+
+    class FlakyCache extends ArrayCache
+    {
+        private int $tries = 0;
+
+        public function set($key, $value, $ttl = null)
+        {
+            $this->tries++;
+            if ($this->tries === 1) {
+                throw new \RuntimeException('fail');
+            }
+
+            return parent::set($key, $value, $ttl);
+        }
     }
 }
